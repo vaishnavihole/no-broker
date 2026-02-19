2d0cbf7d7c845ff8eda89d35eac7d08465b377cb
vaishnavi-cadre <vaishnavi.hole@thecadre.in>
2026-02-19 15:05:21 +0530

    react-project

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import RentalAgreement from './RentalAgreement';
+
+function App() {
+  return (
+    <div className="App">
+      <RentalAgreement />
+    </div>
+  );
+}
+
+export default App;
